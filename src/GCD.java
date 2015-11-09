fa1bce6674e0821a1b303ef5e1e76f6f0d670af6
dupanov <vadim.dupanov@gmail.com>
2015-11-09 02:41:42 +0200

    Rational numnbers objects

--- src/GCD.java
+++ src/GCD.java
@@ -7,13 +7,13 @@ public class GCD {
     public static void main(String args[]){
         for(int i=1; i<100; i++ ){
             for(int j = 1; j<100; j++){
-        System.out.print(i+" "+j+" "+ GCD(i,j));
+        System.out.print(i+" "+j+" "+ gcd(i,j));
                 System.out.println();
             }
         }
     }
 
-    public static int GCD(int a, int b) {
+    public static int gcd(int a, int b) {
         if(a<b){
             int interim = a;
             a = b;
@@ -21,6 +21,6 @@ public class GCD {
         }
         if(a == 0){return b;}
         else if(b==0){return a;}
-       return GCD(b, a%b);
+       return gcd(b, a%b);
     }
 }
